31e0d00a1ac38db34958e4e181b9254b5997b800
kacper-arendt <kacykvaa@gmail.com>
2021-10-07 09:43:39 +0200

    feat add glbal styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import {createGlobalStyle} from 'styled-components';
+import {SearchBar} from './components/SearchBar';
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    width: 100%;
+    height: 100%;
+    font-family: 'Lato', sans-serif;
+  }
+
+  body {
+    box-sizing: border-box;
+  }
+`;
 
 
 function App() {
   return (
-<></>
+      <>
+        <GlobalStyles />
+        <SearchBar/>
+      </>
   );
 }
 
